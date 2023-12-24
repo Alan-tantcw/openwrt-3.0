69a6e7f8f9701b166293c41f7690473e907b0d3f
Alan <152454388+Alan-tantcw@users.noreply.github.com>
2023-12-24 20:51:21 +0800

    Update diy-mini.sh

--- diy-mini.sh
+++ diy-mini.sh
@@ -16,7 +16,6 @@ rm -rf feeds/packages/net/smartdns
 rm -rf feeds/luci/themes/luci-theme-argon
 rm -rf feeds/luci/applications/luci-app-mosdns
 rm -rf feeds/luci/applications/luci-app-netdata
-rm -rf feeds/luci/applications/luci-app-ipsec-vpnserver
 
 # 添加额外插件
 git clone --depth=1 https://github.com/kongfl888/luci-app-adguardhome package/luci-app-adguardhome
