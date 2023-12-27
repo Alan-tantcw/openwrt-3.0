6504559f96d7ccd46d6463551a7a640300ba88b7
Alan <152454388+Alan-tantcw@users.noreply.github.com>
2023-12-27 08:19:57 +0800

    Update diy-mini.sh

--- diy-mini.sh
+++ diy-mini.sh
@@ -25,7 +25,7 @@ rm -rf feeds/luci/applications/luci-app-netdata
 # 腾讯云DDNS
 git clone --depth=1 https://github.com/Alan-tantcw/luci-app-tencentddns package/luci-app-tencentddns
 # ipsec插件
-# git clone --depth=1 https://github.com/Ivaneus/luci-app-ipsec-vpnserver-manyusers package/luci-app-ipsec-vpnserver-manyusers
+svn export https://github.com/Lienol/openwrt-package/trunk/luci-app-ipsec-server package/luci-app-ipsec-server
 # softEther (不启用)
 svn export https://github.com/Lienol/openwrt-package/trunk/luci-app-softethervpn package/luci-app-softethervpn
 # passwall
