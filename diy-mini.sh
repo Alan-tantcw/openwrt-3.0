9fa778c12c4a668476f6d75b65abff88c0d40bc1
Alan <152454388+Alan-tantcw@users.noreply.github.com>
2023-12-28 16:05:13 +0800

    Update diy-mini.sh

--- diy-mini.sh
+++ diy-mini.sh
@@ -30,14 +30,14 @@ svn export https://github.com/Lienol/openwrt-package/trunk/luci-app-ipsec-server
 # softEther (不启用)
 svn export https://github.com/Lienol/openwrt-package/trunk/luci-app-softethervpn package/luci-app-softethervpn
 # passwall
-svn export https://github.com/xiaorouji/openwrt-passwall/trunk/luci-app-passwall package/luci-app-passwall
+svn export https://github.com/NueXini/NueXini_Packages/trunk/luci-app-passwall package/luci-app-passwall
 # vssr 插件  
 # git clone --depth=1 https://github.com/dqylyln/luci-app-vssr package/luci-app-vssr
 
 # 科学上网插件依赖
 # git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall-packages package/openwrt-passwall
-echo 'src-git openwrt-passwall https://github.com/xiaorouji/openwrt-passwall-packages' >> feeds.conf.default
-# echo 'src-git helloworld https://github.com/fw876/helloworld' >> feeds.conf.default
+# echo 'src-git openwrt-passwall https://github.com/xiaorouji/openwrt-passwall-packages' >> feeds.conf.default
+echo 'src-git helloworld https://github.com/fw876/helloworld' >> feeds.conf.default
 
 # 添加额外插件
 git clone --depth=1 https://github.com/kongfl888/luci-app-adguardhome package/luci-app-adguardhome
