7537ba4dfb101c636cb318d61822753e1c9b45c2
Alan <152454388+Alan-tantcw@users.noreply.github.com>
2024-01-02 08:37:22 +0800

    Update diy-mini.sh

--- diy-mini.sh
+++ diy-mini.sh
@@ -28,7 +28,7 @@ svn export https://github.com/Lienol/openwrt-package/trunk/luci-app-ipsec-server
 # softEther (不启用)
 svn export https://github.com/Lienol/openwrt-package/trunk/luci-app-softethervpn package/luci-app-softethervpn
 # passwall
-git clone --depth=1 https://github.com/kenzok8/small package/openwrt-packagesAlan
+# git clone --depth=1 https://github.com/kenzok8/small package/openwrt-packagesAlan    --去掉ssr
 rm -rf {*ssr*,*bypass*,*vssr*}
 rm -rf feeds/packages/openwrt-packagesAlan/luci-app-passwall2
 rm -rf feeds/packages/openwrt-packagesAlan/luci-app-ssr-plus
