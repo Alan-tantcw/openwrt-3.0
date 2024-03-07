abbca67418b518e1c9c740286773cd8ff428d300
Alan <152454388+Alan-tantcw@users.noreply.github.com>
2024-03-07 10:26:13 +0800

    Update diy-mini.sh

--- diy-mini.sh
+++ diy-mini.sh
@@ -3,7 +3,7 @@
 # 修改默认IP
 sed -i 's/192.168.1.1/11.1.1.1/g' package/base-files/files/bin/config_generate
 # 禁用ipv6
-sed -i "/uci set network\.lan\.ip6assign='60'/d" package/base-files/files/bin/config_generate
+sed -i "/ip6assign='60'/d" package/base-files/files/bin/config_generate
 
 # 更改默认 Shell 为 zsh
 # sed -i 's/\/bin\/ash/\/usr\/bin\/zsh/g' package/base-files/files/etc/passwd
