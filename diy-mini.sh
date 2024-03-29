bdb682a30a2a726a06d6a3857513e809657e9bc3
Alan <152454388+Alan-tantcw@users.noreply.github.com>
2024-03-29 09:28:37 +0800

    Update diy-mini.sh

--- diy-mini.sh
+++ diy-mini.sh
@@ -41,7 +41,11 @@ svn export https://github.com/Lienol/openwrt-package/trunk/luci-app-ipsec-server
 svn export https://github.com/Lienol/openwrt-package/trunk/luci-app-softethervpn package/luci-app-softethervpn
 # 引用其他源-----------------------------------------------luci-app-passwall-----------------------------------------------
 # 科学上网插件
-
+git clone --depth=1 -b main https://github.com/fw876/helloworld package/luci-app-ssr-plus
+git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall-packages package/openwrt-passwall
+git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall package/luci-app-passwall
+git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall2 package/luci-app-passwall2
+git_sparse_clone master https://github.com/vernesong/OpenClash luci-app-openclash
 
 # 添加额外插件
 git clone --depth=1 https://github.com/kongfl888/luci-app-adguardhome package/luci-app-adguardhome
