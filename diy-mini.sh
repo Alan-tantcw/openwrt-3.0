9486422dd5a1638a33158e4fbdc6660509ff02ab
Alan <152454388+Alan-tantcw@users.noreply.github.com>
2024-04-02 14:28:26 +0800

    Update diy-mini.sh

--- diy-mini.sh
+++ diy-mini.sh
@@ -20,9 +20,7 @@ rm -rf feeds/luci/applications/luci-app-mosdns
 rm -rf feeds/luci/applications/luci-app-netdata
 
 # kenzok8依赖清除，防止冲突
-rm -rf feeds/packages/net/{xray*,v2ray*,v2ray*,sing*,smartdns}
-rm -rf feeds/packages/utils/v2dat
-rm -rf feeds/packages/lang/golang
+# rm -rf feeds/packages/net/{xray*,v2ray*,v2ray*,sing*,smartdns}
 
 
 
