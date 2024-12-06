27b91b1ed28607145dc55b0cc39e33c53f6071f8
Alan <152454388+Alan-tantcw@users.noreply.github.com>
2024-12-06 14:59:52 +0800

    Update diy-mini.sh

--- diy-mini.sh
+++ diy-mini.sh
@@ -90,7 +90,7 @@ git clone --depth=1 https://github.com/kongfl888/luci-app-adguardhome package/lu
 git clone --depth=1 -b openwrt-24.10 https://github.com/sbwml/luci-theme-argon.git package/luci-theme-argon
 
 # 更改 Argon 主题背景
-cp -f $GITHUB_WORKSPACE/images/bg.webp package/luci-theme-argon/htdocs/luci-static/argon/img/bg.webp
+cp -f $GITHUB_WORKSPACE/images/bg.webp package/luci-theme-argon/luci-theme-argon/htdocs/luci-static/argon/img/bg.webp
 
 # SmartDNS
 git clone --depth=1 -b lede https://github.com/pymumu/luci-app-smartdns package/luci-app-smartdns
