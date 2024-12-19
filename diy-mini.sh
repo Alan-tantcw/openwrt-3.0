9c2662fde43f57c452379d6ad674a0af9b839d11
Alan <152454388+Alan-tantcw@users.noreply.github.com>
2024-12-19 15:58:22 +0800

    Update diy-mini.sh

--- diy-mini.sh
+++ diy-mini.sh
@@ -3,7 +3,7 @@
 
 
 # 修改默认IP
-sed -i 's/192.168.1.1/11.1.1.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192\.168\.1\.1/11.1.1.1/g' package/base-files/files/bin/config_generate
 # 禁用ipv6
 sed -i "/ip6assign='60'/d" package/base-files/files/bin/config_generate
 
